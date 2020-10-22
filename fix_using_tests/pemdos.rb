02983c7992e22c46f3841d71ad6ae5727c8a9d12
Joshua Frank <eurosewusa@gmail.com>
2020-10-22 13:32:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
-# don't forget to add: require 'pry'
+# don't forget to add:
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    return 10 * "s" + string
   else
-    string
+   return string
   end
 end
